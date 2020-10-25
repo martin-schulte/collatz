ce9a56370c9d71e379186851683d9b045b500660
Martin Schulte <martin.schulte@schrader-schulte.de>
2020-10-25 09:20:52 +0100

    - corrected bug that only last element of solution was checked for being integer - round to 5 digits to avoid rounding problems - show operations - insert empty lines into the output

--- collatz-lin.py
+++ collatz-lin.py
@@ -2,32 +2,40 @@
 
 import numpy as np
 
+printit=False
+
 for n in range(5, 6):
 #  print( n )
   for op in range(0, 2**n):
+    if not printit:
+      printit=True
+    else:
+      print()
 #    print( "  "+str(op) )
     a = np.zeros([n,n], dtype=int)
     b = np.zeros([n], dtype=int)
-    opstr=""
+    ops=[]
     for r in range(0, n):
       if op >> r & 1 == 0:
-#        print( "    np/2" )
+        ops.append( "np/2" )
         a[r][r]=1
         a[r][(r+1)%n]=-2
       else:
-#        print( "    3n+1" )
+        ops.append( "3n+1" )
         a[r][r]=-3
         a[r][(r+1)%n]=1
         b[r]=1
 #    print( a, b)
     x=np.linalg.solve(a,b)
-    print(x)
     is_int=True
     for pos in range(0, n):
-      if not x[r].is_integer():
+      x[pos]=round(x[pos], 5)
+      if not x[pos].is_integer():
+#        print( "No int: "+("{:.20f}".format(x[pos])) )
         is_int=False
         break
     if is_int:
-      print( "Dimension:  "+str(n) )
-      print( "Operations: " )
-      print( "Solution:   "+str(x) )
+      print( "Working:" )
+    print( "Dimension:  "+str(n) )
+    print( "Operations: "+str(ops) )
+    print( "Solution:   "+str(x) )
